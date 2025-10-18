b7e707fa4e8ef2ee2f65c64a47ef875e1bf75467
kodratIT <kodratcoc@gmail.com>
2025-10-18 14:29:39 +0700

    feat(STORY-003): create @repo/eslint-config package
    - Add package.json for @repo/eslint-config
    - Create base.js with TypeScript and Prettier integration
    - Create next.js config extending base
    - Create react.js config for React libraries
    
    Task 2/10 completed

---
+++ packages/config/eslint-config/base.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+};
---
+++ packages/config/eslint-config/next.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    './base.js',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/no-unescaped-entities': 'off',
+  },
+};
