18ccba84834eebd152b2ceb0ea327d50ef774e5c
kodratIT <kodratcoc@gmail.com>
2025-10-19 10:45:56 +0700

    feat(STORY-006): add package exports
    - Export all components (Button, Card, Input)
    - Export all component prop types
    - Export utility functions (cn, getInitials)
    - Ready for consumption by other packages
    
    Task 5/5 completed - STORY-006 COMPLETE

---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,20 @@
+// Components
+export { Button, type ButtonProps } from './components/button';
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  type CardProps,
+  type CardHeaderProps,
+  type CardTitleProps,
+  type CardDescriptionProps,
+  type CardContentProps,
+  type CardFooterProps,
+} from './components/card';
+export { Input, type InputProps } from './components/input';
+
+// Utils
+export { cn, getInitials } from './lib/utils';
